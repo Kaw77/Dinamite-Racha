157b613219729f01ee3865dda6dccff8b8fb811a
Kaw77 <c.wagner.oliveira@gmail.com>
2026-06-21 20:17:13 -0300

    ajustando posição e movimento do jogador arquivos não tem a dimensão certa

--- main.py
+++ main.py
@@ -1,128 +1,51 @@
-'''
-
-
-
-
-# Relógio
-clock = pygame.time.Clock()
-
-
-
-# Som de colisão
-explosion_sound = pygame.mixer.Sound(
-    "assets/sons/colisao.wav"
-)
-
-# Música do menu
-pygame.mixer.music.load(
-    "assets/sons/menu.mp3"
-)
-
-pygame.mixer.music.play(-1)
-
-# Fundo atual
-fundo_atual = fundo_menu
-
-rodando = True
-
-while rodando:
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            rodando = False
-
-        if event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_1:
-                fundo_atual = fase1
-
-            elif event.key == pygame.K_2:
-                fundo_atual = fase2
-
-            elif event.key == pygame.K_3:
-                fundo_atual = fase3
-
-            elif event.key == pygame.K_4:
-                fundo_atual = fase4
-
-    # Desenha fundo
-    window.blit(fundo_atual, (0, 0))
-
-    pygame.display.update()
-
-    clock.tick(60)
-
-pygame.quit()'''
-#teste
-# Inicialização
 import pygame
+from src.jogador import Jogador
 
 pygame.init()
 
-# Janela
 LARGURA = 800
 ALTURA = 600
 
-tela = pygame.display.set_mode((LARGURA,ALTURA))
+tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Dinamite Racha")
 
-# Fundo do menu
 fundo = pygame.image.load(
     "assets/imagens/dinamite.png"
 )
 
 fundo = pygame.transform.scale(
     fundo,
-    (800, 600)
-)
-
-# Fundos das fases
-fase1 = pygame.transform.scale(
-    pygame.image.load("assets/imagens/fase1.png"),
     (LARGURA, ALTURA)
 )
 
-fase2 = pygame.transform.scale(
-    pygame.image.load("assets/imagens/fase2.png"),
-    (LARGURA, ALTURA)    
-)
-
-fase3 = pygame.transform.scale(
-    pygame.image.load("assets/imagens/fase3.png"),
-    (LARGURA, ALTURA)
-)
-
-fase4 = pygame.transform.scale(
-    pygame.image.load("assets/imagens/fase4.png"),
-    (LARGURA, ALTURA)
-)
-
-from src.jogador import Jogador
-
+# Jogador começa na pista 1
 jogador = Jogador(
-    "assets/imagens/jogador1.png",
-    350,
-    450
+    "assets/imagens/jogadores/jogador1.png",
+    80,
+    350
 )
+
+clock = pygame.time.Clock()
 
 rodando = True
 
 while rodando:
+
+    clock.tick(60)
 
     for evento in pygame.event.get():
 
         if evento.type == pygame.QUIT:
             rodando = False
 
-    tela.blit(fundo, (0,0))
+    teclas = pygame.key.get_pressed()
+
+    jogador.mover(teclas)
+
+    tela.blit(fundo, (0, 0))
+
+    jogador.desenhar(tela)
 
     pygame.display.flip()
-    
-    window.blit(fundo_menu, (0,0))
-
-jogador.desenhar(window)
-
-pygame.display.update()
 
 pygame.quit()
--- src/jogador.py
+++ src/jogador.py
@@ -3,27 +3,74 @@ import os
 
 class Jogador:
 
+    LARGURA_NAVE = 56
+    ALTURA_NAVE = 56
+
     def __init__(self, imagem, x, y):
 
         caminho = os.path.join(os.getcwd(), imagem)
 
         if os.path.exists(caminho):
             self.imagem = pygame.image.load(caminho).convert_alpha()
+
             self.imagem = pygame.transform.scale(
                 self.imagem,
-                (64, 64)
+                (self.LARGURA_NAVE, self.ALTURA_NAVE)
             )
         else:
-            print("Imagem não encontrada:", caminho)
-            self.imagem = pygame.Surface((64, 64))
+            self.imagem = pygame.Surface(
+                (self.LARGURA_NAVE, self.ALTURA_NAVE)
+            )
             self.imagem.fill((255, 0, 0))
 
         self.x = x
         self.y = y
+
         self.velocidade = 5
 
+    def centralizar_na_pista(self, topo_pista, altura_pista):
+
+        self.y = topo_pista + (
+            altura_pista - self.ALTURA_NAVE
+        ) // 2
+
     def desenhar(self, tela):
-        tela.blit(self.imagem, (self.x, self.y))
+
+        tela.blit(
+            self.imagem,
+            (self.x, self.y)
+        )
 
     def obter_rect(self):
-        return self.imagem.get_rect(topleft=(self.x, self.y))
+
+        return self.imagem.get_rect(
+            topleft=(self.x, self.y)
+        )
+        
+    def mover(self, teclas):
+
+        if teclas[pygame.K_LEFT]:
+           self.x -= self.velocidade
+
+        if teclas[pygame.K_RIGHT]:
+           self.x += self.velocidade
+
+        if teclas[pygame.K_UP]:
+           self.y -= self.velocidade
+
+        if teclas[pygame.K_DOWN]:
+           self.y += self.velocidade
+
+    # Limites da tela
+
+        if self.x < 0:
+           self.x = 0
+
+        if self.x > 800 - self.imagem.get_width():
+           self.x = 800 - self.imagem.get_width()
+
+        if self.y < 0:
+           self.y = 0
+
+        if self.y > 600 - self.imagem.get_height():
+           self.y = 600 - self.imagem.get_height()
