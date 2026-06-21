3c5027baf95d1adfd9fc4d0ed1c843e771eca281
Kaw77 <c.wagner.oliveira@gmail.com>
2026-06-21 18:59:06 -0300

    som e imagem da explosão

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ surface_fase1 = pygame.image.load("assets/imagens/fase1.png")
 surface_fase2 = pygame.image.load("assets/imagens/fase2.png")
 surface_fase3 = pygame.image.load("assets/imagens/fase3.png")
 surface_fase4 = pygame.image.load("assets/imagens/fase4.png")
+importar a explosão para colisão
+explosion_image = pygame.image.load("assets/imagens/explosao.png")
 
 # Obter o retângulo do fundo
 rect = surface.get_rect()
@@ -41,12 +43,18 @@ clock = pygame.time.Clock()
 pygame.mixer.music.load("assets/sons/menu.mp3")
 pygame.mixer.music.play(-1)
 # Carregar o som da explosão
-explosion_sound = pygame.mixer.Sound("assets/sons/explosion.mp3")
+explosion_sound = pygame.mixer.Sound("assets/sons/colisao.wav")
 #importar sons para a fase 1, fase 2, fase 3 e fase 4
 fase1_sound = pygame.mixer.Sound("assets/sons/fase1.mp3")
 fase2_sound = pygame.mixer.Sound("assets/sons/fase2.mp3")
 fase3_sound = pygame.mixer.Sound("assets/sons/fase3.mp3")
 fase4_sound = pygame.mixer.Sound("assets/sons/fase4.mp3")
+#Tocar o som da fase 1
+fase1_sound.play()
+fase2_sound.play()
+fase3_sound.play()
+fase4_sound.play()
+
 
 #Ajustar o fps do jogo
 clock.tick(60)  
